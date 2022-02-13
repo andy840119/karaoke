3323bce52f4a521b4694d73a48825f3795f59a58
andy840119 <andy840119@gmail.com>
2022-02-13 21:34:41 +0800

    temp.

--- osu.Game.Rulesets.Karaoke.Tests/Editor/ChangeHandlers/BaseBeatmapPropertyChangeHandlerTest.cs
+++ osu.Game.Rulesets.Karaoke.Tests/Editor/ChangeHandlers/BaseBeatmapPropertyChangeHandlerTest.cs
@@ -1,13 +1,23 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers;
 
 namespace osu.Game.Rulesets.Karaoke.Tests.Editor.ChangeHandlers
 {
-    public class BaseBeatmapPropertyChangeHandlerTest<TChangeHandler, TProperty> : BaseChangeHandlerTest<TChangeHandler>
+    public abstract class BaseBeatmapPropertyChangeHandlerTest<TChangeHandler, TProperty> : BaseChangeHandlerTest<TChangeHandler>
         where TChangeHandler : BeatmapPropertyChangeHandler<TProperty>, new()
     {
+        protected void PrepareBeatmapProperty(TProperty property)
+            => PrepareBeatmapProperties(new[] { property });
 
+        protected void PrepareBeatmapProperties(IEnumerable<TProperty> listOfProperties)
+        {
+            AddStep("Prepare testing beatmap properties", () =>
+            {
+                // todo: add the properties into karaoke beatmap.
+            });
+        }
     }
 }
