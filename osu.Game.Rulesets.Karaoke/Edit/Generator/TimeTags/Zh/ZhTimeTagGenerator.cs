f96fb8eaaabc722dbfbd9f333e4de4079c78b441
andy840119 <andy840119@gmail.com>
2022-05-22 23:04:11 +0800

    Implement the logic for checking the spacing.

--- osu.Game.Rulesets.Karaoke/Edit/Generator/TimeTags/Zh/ZhTimeTagGenerator.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Generator/TimeTags/Zh/ZhTimeTagGenerator.cs
@@ -21,10 +21,18 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Generator.TimeTags.Zh
 
             for (int i = 1; i < text.Length; i++)
             {
-                if (CharUtils.IsChinese(text[i]))
+                char c = text[i];
+                if (CharUtils.IsSpacing(c))
+                    continue;
+
+                if (CharUtils.IsChinese(c))
                 {
                     timeTags.Add(new TimeTag(new TextIndex(i)));
                 }
+
+                // should add end index if next char is space.
+                if (i + 1 < text.Length && CharUtils.IsSpacing(text[i + 1]))
+                    timeTags.Add(new TimeTag(new TextIndex(i, TextIndex.IndexState.End)));
             }
         }
     }
