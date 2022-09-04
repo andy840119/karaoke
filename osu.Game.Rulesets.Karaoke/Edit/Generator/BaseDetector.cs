0efd302d098269886701e40e5a28fe5bbdf94111
andy840119 <andy840119@gmail.com>
2022-09-04 15:38:11 +0800

    Implement the base class. Will write all basic check logic in the base class.

---
+++ osu.Game.Rulesets.Karaoke/Edit/Generator/BaseDetector.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Framework.Localisation;
+using osu.Game.Rulesets.Karaoke.Edit.Generator.Types;
+using osu.Game.Rulesets.Karaoke.Edit.Utils;
+using osu.Game.Rulesets.Karaoke.Objects;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Generator
+{
+    public abstract class BaseDetector<TProperty> : ILyricPropertyDetector<TProperty>
+    {
+        public LocalisableString? GetInvalidMessage(Lyric lyric)
+        {
+            var locked = HitObjectWritableUtils.GetLyricPropertyLockedReason(lyric, DetectPropertyName);
+            return locked switch
+            {
+                LockLyricPropertyBy.ReferenceLyricConfig => "Cannot detect property because the property is sync from other lyric.",
+                LockLyricPropertyBy.LockState => "Cannot detect property because the property is locked.",
+                _ => GetExtraCheck(lyric)
+            };
+        }
+
+        public TProperty Detect(Lyric lyric)
+        {
+            if (GetInvalidMessage(lyric) != null)
+                throw new NotSupportedException();
+
+            return PerformDetect(lyric);
+        }
+
+        protected abstract string[] DetectPropertyName { get; }
+
+        protected virtual LocalisableString? GetExtraCheck(Lyric lyric)
+        {
+            return null;
+        }
+
+        protected abstract TProperty PerformDetect(Lyric lyric);
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/Edit/Generator/BaseGenerator.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Framework.Localisation;
+using osu.Game.Rulesets.Karaoke.Edit.Generator.Types;
+using osu.Game.Rulesets.Karaoke.Edit.Utils;
+using osu.Game.Rulesets.Karaoke.Objects;
+
+namespace osu.Game.Rulesets.Karaoke.Edit.Generator
+{
+    public abstract class BaseGenerator<TProperty> : ILyricPropertyGenerator<TProperty>
+    {
+        public LocalisableString? GetInvalidMessage(Lyric lyric)
+        {
+            var locked = HitObjectWritableUtils.GetLyricPropertyLockedReason(lyric, GeneratePropertyName);
+            return locked switch
+            {
+                LockLyricPropertyBy.ReferenceLyricConfig => "Cannot generate property because the property is sync from other lyric.",
+                LockLyricPropertyBy.LockState => "Cannot generate property because the property is locked.",
+                _ => GetExtraCheck(lyric)
+            };
+        }
+
+        public TProperty Generate(Lyric lyric)
+        {
+            if (GetInvalidMessage(lyric) != null)
+                throw new NotSupportedException();
+
+            return PerformGenerate(lyric);
+        }
+
+        protected abstract string[] GeneratePropertyName { get; }
+
+        protected virtual LocalisableString? GetExtraCheck(Lyric lyric)
+        {
+            return null;
+        }
+
+        protected abstract TProperty PerformGenerate(Lyric lyric);
+    }
+}
