a0d04ee7486b9c023f7fdac6dcbde2964b9a96d8
andy840119 <andy840119@gmail.com>
2023-03-13 23:02:03 +0800

    damn merge.

--- osu.Game.Rulesets.Karaoke/Screens/Edit/Stages/Classic/Stage/ClassicLyricLayoutPopover.cs
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/Stages/Classic/Stage/ClassicLyricLayoutPopover.cs
@@ -1,7 +1,10 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
 using osu.Framework.Localisation;
+using osu.Game.Graphics.Containers;
 using osu.Game.Graphics.UserInterfaceV2;
 using osu.Game.Rulesets.Karaoke.Beatmaps.Stages.Classic;
 using osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers.Beatmaps;
@@ -12,7 +15,31 @@ public partial class ClassicLyricLayoutPopover : OsuPopover
 {
     public ClassicLyricLayoutPopover(ClassicLyricLayout classicLyricLayout)
     {
-
+        Child = new GridContainer
+        {
+            Height = 500,
+            Width = 300,
+            ColumnDimensions = new[]
+            {
+                new Dimension(GridSizeMode.Relative, 300),
+                new Dimension(GridSizeMode.AutoSize),
+            },
+            Content = new[]
+            {
+                new[]
+                {
+                    new OsuScrollContainer
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Children = new[]
+                        {
+                            new LyricLayoutSection(classicLyricLayout)
+                        }
+                    },
+                    // todo: place the preview area in here.
+                }
+            }
+        };
     }
 
     public partial class LyricLayoutSection : EditorSection
