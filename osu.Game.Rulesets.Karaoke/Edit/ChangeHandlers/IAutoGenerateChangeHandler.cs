36b67418a01f68877b0bcdf06ea59bcac47ac851
andy840119 <andy840119@gmail.com>
2023-07-23 16:37:31 +0800

    Define the type for use different generator by type.

--- osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/IAutoGenerateChangeHandler.cs
+++ osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/IAutoGenerateChangeHandler.cs
@@ -1,6 +1,8 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+
 namespace osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers;
 
 /// <summary>
@@ -12,6 +14,17 @@ public interface ITypeAutoGenerateChangeHandler<in TType> where TType : notnull
     bool CanGenerate<T>() where T : TType;
 
     void AutoGenerate<T>() where T : TType;
+}
+
+/// <summary>
+/// This interface is defined checking able to generate or detect the property, and make the change for the property.
+/// </summary>
+/// <typeparam name="TEnum"></typeparam>
+public interface IEnumAutoGenerateChangeHandler<in TEnum> where TEnum : Enum
+{
+    bool CanGenerate(TEnum type);
+
+    void AutoGenerate(TEnum type);
 }
 
 /// <summary>
--- osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/Lyrics/ILyricPropertyAutoGenerateChangeHandler.cs
+++ osu.Game.Rulesets.Karaoke/Edit/ChangeHandlers/Lyrics/ILyricPropertyAutoGenerateChangeHandler.cs
@@ -1,11 +1,17 @@
 // Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
 using osu.Framework.Localisation;
 using osu.Game.Rulesets.Karaoke.Objects;
 
 namespace osu.Game.Rulesets.Karaoke.Edit.ChangeHandlers.Lyrics;
+
+public interface ILyricPropertyEnumAutoGenerateChangeHandler<in TEnum> : IEnumAutoGenerateChangeHandler<TEnum> where TEnum : Enum
+{
+    IDictionary<Lyric, LocalisableString> GetGeneratorNotSupportedLyrics(TEnum type);
+}
 
 public interface ILyricPropertyAutoGenerateChangeHandler : IAutoGenerateChangeHandler
 {
