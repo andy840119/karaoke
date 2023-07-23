e983cf3f9282ca7213a7b3a09a4960db314e7675
andy840119 <andy840119@gmail.com>
2023-07-23 16:45:55 +0800

    Implement the auto-generate button that support different generate type.

--- osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Lyrics/Settings/LyricEditorAutoGenerateSubsection.cs
+++ osu.Game.Rulesets.Karaoke/Screens/Edit/Beatmaps/Lyrics/Settings/LyricEditorAutoGenerateSubsection.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) andy840119 <andy840119@gmail.com>. Licensed under the GPL Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
 using osu.Framework.Allocation;
 using osu.Framework.Localisation;
@@ -37,6 +38,51 @@ public abstract partial class LyricEditorAutoGenerateSubsection<TChangeHandler> 
         protected override void Apply()
         {
             changeHandler.AutoGenerate();
+        }
+    }
+}
+
+public abstract partial class LyricEditorAutoGenerateSubsection<TChangeHandler, TType> : AutoGenerateSubsection
+    where TChangeHandler : class, ILyricPropertyEnumAutoGenerateChangeHandler<TType>
+    where TType : Enum
+{
+    private readonly TType type;
+
+    protected LyricEditorAutoGenerateSubsection(TType type)
+    {
+        this.type = type;
+    }
+
+    protected override EditorSectionButton CreateGenerateButton()
+        => new AutoGenerateButton(type);
+
+    protected sealed override DescriptionTextFlowContainer CreateDescriptionTextFlowContainer()
+        => new LyricEditorDescriptionTextFlowContainer();
+
+    private partial class AutoGenerateButton : SelectLyricButton
+    {
+        [Resolved]
+        private TChangeHandler changeHandler { get; set; } = null!;
+
+        protected override LocalisableString StandardText => "Generate";
+
+        protected override LocalisableString SelectingText => "Cancel generate";
+
+        private readonly TType type;
+
+        public AutoGenerateButton(TType type)
+        {
+            this.type = type;
+        }
+
+        protected override IDictionary<Lyric, LocalisableString> GetDisableSelectingLyrics()
+        {
+            return changeHandler.GetGeneratorNotSupportedLyrics(type);
+        }
+
+        protected override void Apply()
+        {
+            changeHandler.AutoGenerate(type);
         }
     }
 }
