0688140df1d35720b73569f82d3c58e8905e948b
andy840119 <andy840119@gmail.com>
2024-12-08 17:37:08 +0800

    Apply the style to the provider.

--- osu.Game.Rulesets.Karaoke/Stages/Infos/Preview/PreviewLyricCommandProvider.cs
+++ osu.Game.Rulesets.Karaoke/Stages/Infos/Preview/PreviewLyricCommandProvider.cs
@@ -4,8 +4,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using osu.Framework.Graphics;
 using osu.Game.Rulesets.Karaoke.Objects;
 using osu.Game.Rulesets.Karaoke.Stages.Commands;
+using osu.Game.Rulesets.Karaoke.Stages.Commands.Lyrics;
 using osu.Game.Rulesets.Objects.Drawables;
 
 namespace osu.Game.Rulesets.Karaoke.Stages.Infos.Preview;
@@ -34,7 +36,7 @@ public class PreviewLyricCommandProvider : HitObjectCommandProvider<PreviewStage
         return elements.Select(e => e switch
         {
             PreviewLyricLayout previewLyricLayout => updateInitialTransforms(previewLyricLayout),
-            PreviewStyle => Array.Empty<IStageCommand>(), // todo: implement.
+            PreviewStyle style => updateInitialTransforms(style),
             _ => throw new NotSupportedException(),
         }).SelectMany(x => x);
     }
@@ -92,6 +94,14 @@ public class PreviewLyricCommandProvider : HitObjectCommandProvider<PreviewStage
         static bool isFirstLyricInView(PreviewLyricLayout layout) => !layout.Timings.Any();
 
         static bool isLastLyricInView(PreviewLyricLayout layout) => layout.StartTime != 0;
+    }
+
+    private IEnumerable<IStageCommand> updateInitialTransforms(PreviewStyle style)
+    {
+        if (style.LyricFontInfo != null)
+        {
+            yield return new LyricFontInfoCommand(Easing.None, 0, 0, style.LyricFontInfo, style.LyricFontInfo);
+        }
     }
 
     protected override IEnumerable<IStageCommand> GetStartTimeStateCommands(Lyric hitObject)
